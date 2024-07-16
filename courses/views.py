f07381737558565834b22cb682e6b62444d32877
hatsimitsu <onusha45@gmail.com>
2024-07-16 15:21:01 +0545

    course initialized

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
+from .models import Course
 # Create your views here.
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+  
+    courses = Course.objects.all()
+    print(courses)
+    context = {
+        "courses" :courses,
+    }
+    return render(request,'index.html',context)
